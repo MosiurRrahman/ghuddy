e980854c140b95f6104b8daa16a187c63118d96b
MosiurRrahman <mosiurrahman595@gmail.com>
2022-11-05 15:29:30 +0600

    image gelerator pdf

--- main.js
+++ main.js
@@ -97,44 +97,43 @@ jQuery.getJSON("./data.json", function (result) {
       `;
 
   $("#test").html(pdfData);
+ 
 });
 
 
 $("#btn").click(function () {
-  function getPDF() {
-    var HTML_Width = $("#test").width();
-    var HTML_Height = $("#test").height();
-    var top_left_margin = 1;
-    var PDF_Width = HTML_Width + top_left_margin * 2;
-    var PDF_Height = PDF_Width * 1.5 + top_left_margin * 2;
-    var canvas_image_width = HTML_Width;
-    var canvas_image_height = HTML_Height;
+  function getPDF(){
 
-    var totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
+		var HTML_Width = $("#test").width();
+		var HTML_Height = $("#test").height();
+		var top_left_margin = 15;
+		var PDF_Width = HTML_Width+(top_left_margin*2);
+		var PDF_Height = (PDF_Width*1.5)+(top_left_margin*2);
+		var canvas_image_width = HTML_Width;
+		var canvas_image_height = HTML_Height;
+		
+		var totalPDFPages = Math.ceil(HTML_Height/PDF_Height)-1;
+		
 
-    html2canvas($("#test")[0], { allowTaint: true }).then(function (canvas) {
-      canvas.getContext("2d");
-
-      console.log(canvas.height + "  " + canvas.width);
-
-      var imgData = canvas.toDataURL("image/jpeg", 1.0);
-      var pdf = new jsPDF("p", "pt", [PDF_Width, PDF_Height]);
-
-
-      for (var i = 1; i <= totalPDFPages; i++) {
-        pdf.addPage(PDF_Width, PDF_Height);
-        pdf.addImage(
-          imgData,
-          "JPG",
-          top_left_margin,
-          -(PDF_Height * i) + top_left_margin * 4,
-          canvas_image_width,
-          canvas_image_height
-        );
-      }
-
-      pdf.save("HTML-Document.pdf");
-    });
-  }
+		html2canvas($("#test")[0],{allowTaint:true}).then(function(canvas) {
+			canvas.getContext('2d');
+			
+			console.log(canvas.height+"  "+canvas.width);
+			
+			
+			var imgData = canvas.toDataURL("image/jpeg", 1.0);
+			var pdf = new jsPDF('p', 'pt',  [PDF_Width, PDF_Height]);
+		    pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin,canvas_image_width,canvas_image_height);
+			
+			
+			for (var i = 1; i <= totalPDFPages; i++) { 
+				pdf.addPage(PDF_Width, PDF_Height);
+				pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height*i)+(top_left_margin*4),canvas_image_width,canvas_image_height);
+			}
+			
+		    pdf.save("HTML-Document.pdf");
+        });
+	};
   getPDF();
 });
+
